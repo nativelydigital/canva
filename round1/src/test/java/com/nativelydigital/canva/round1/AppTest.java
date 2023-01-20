0c32b6d9ea7d1555dcacf1e0245986a3f945deff
Matt Cole <120458644+nativelydigital@users.noreply.github.com>
2023-01-21 00:17:28 +0800

    Add hamcrest library for testing

--- round1/src/test/java/com/nativelydigital/canva/round1/AppTest.java
+++ round1/src/test/java/com/nativelydigital/canva/round1/AppTest.java
@@ -1,11 +1,23 @@
 package com.nativelydigital.canva.round1;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class AppTest {
 	@Test
 	public void shouldAnswerWithTrue() {
-		Assertions.assertTrue(true);
+		Assertions.assertTrue(true); // JUnit
+		assertThat(true, equalTo(true)); // Hamcrest
+	}
+	
+	@ParameterizedTest
+	@ValueSource(ints = { 1, 2, 3 })
+	public void positiveNumbersArePositive(int number) {
+		assertThat(number, greaterThan(0));
 	}
 }
