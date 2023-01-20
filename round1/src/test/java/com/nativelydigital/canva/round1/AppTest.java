ffbb6257e4decd0106e07d0b450184849b82a09c
Matt Cole <120458644+nativelydigital@users.noreply.github.com>
2023-01-20 23:09:32 +0800

    Upgrade JUnit from 4 to 5

--- round1/src/test/java/com/nativelydigital/canva/round1/AppTest.java
+++ round1/src/test/java/com/nativelydigital/canva/round1/AppTest.java
@@ -1,20 +1,13 @@
 package com.nativelydigital.canva.round1;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        Assertions.assertTrue( true );
     }
 }
